71d2cbfa1d870f405b994183360c4341e9c6df7d
jconkle <jconkle@scvl.com>
2019-12-26 11:44:54 -0600

    added humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.',undefined)
         } else {
-            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + ' chance of rain.')
+            callback(undefined,body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.  There is a ' + body.currently.precipProbability + ' chance of rain. Humidty for today is '+ body.daily.data[0].humidity )
         }
     })
 }
